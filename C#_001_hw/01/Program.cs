9e53f6974e31bc52b6578e689f6bed759a17a2df
Aleksei Grinko <als.grinko@yandex.ru>
2022-12-17 18:12:40 +0400

    make example

--- C#_001_hw/01/Program.cs
+++ C#_001_hw/01/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите первое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+if (numberA > numberB)
+{
+    Console.WriteLine($"Первое число {numberA} больше второго числа {numberB}");
+}
+
+else if (numberA < numberB)
+{
+    Console.WriteLine($"Первое число {numberA} меньше второго числа {numberB}");
+}
+
+else 
+{
+    Console.WriteLine($"Первое число {numberA} равно второму числу {numberB}");
+}
