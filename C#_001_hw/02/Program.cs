b48672522bdd7fd08ac439d15f7509c6ca689ed0
Aleksei Grinko <als.grinko@yandex.ru>
2022-12-17 18:19:00 +0400

    make example2

---
+++ C#_001_hw/02/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите первое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите тертье число: ");
+int numberC = Convert.ToInt32(Console.ReadLine());
+
+int max = numberA;
+
+if (max < numberB) 
+{
+    max = numberB;
+}
+if (max < numberC)
+{
+    max = numberC;
+}
+
+Console.WriteLine($"Саммое большое число {max}");
