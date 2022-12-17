e174117125c68583e0a2d16d6a0662073dda2633
Aleksei Grinko <als.grinko@yandex.ru>
2022-12-17 18:32:45 +0400

    make example4

---
+++ C#_001_hw/04/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+
+Console.WriteLine($"Все чётные число до числа {number} включительно: ");
+while (count <= number) {
+    if (count%2 == 0) 
+        {
+            Console.Write($" {count}");
+        }
+    count++;
+}
+Console.Write(".");
+
