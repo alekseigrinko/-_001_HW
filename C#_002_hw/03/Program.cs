34e68ad90d926d025dbd3e8f2453e1f41e699284
Aleksei Grinko <als.grinko@yandex.ru>
2022-12-18 15:58:38 +0400

    make example 3

---
+++ C#_002_hw/03/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите число, порядкового номера дня недели: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 1 || number > 7) 
+{
+    Console.WriteLine($"Число {number} не соответствует ни одному из семи дней недели!");
+}
+
+if (number == 6) 
+{
+    Console.WriteLine($"{number}-й день недели выходной - суббота!");
+}
+
+else if (number == 7) 
+{
+    Console.WriteLine($"{number}-й день недели выходной - воскресенье!");
+}
+
+else 
+{
+    Console.WriteLine($"Вы ввели номер буднего дня недели. До субботы осталось {6 - number} дн.");
+}
