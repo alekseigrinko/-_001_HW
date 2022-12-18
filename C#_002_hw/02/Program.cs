f86e5e961d39676cf0810cc1c24fdaa8a78d050a
Aleksei Grinko <als.grinko@yandex.ru>
2022-12-18 15:47:22 +0400

    make example 2

---
+++ C#_002_hw/02/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+char[] array = number.ToString().ToCharArray();
+
+if (array.Length < 3)
+{
+    Console.WriteLine($"Число {number} не содержит 3-й знак!");
+}
+
+else
+{
+    Console.WriteLine($"Третий знак введённого числа {array[2]}!");
+}
