d300b99e9f3dbef12a9733014834601b459e0941
Aleksei Grinko <als.grinko@yandex.ru>
2022-12-18 15:41:51 +0400

    make example 1

---
+++ C#_002_hw/01/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите трёхзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+char[] array = number.ToString().ToCharArray();
+
+if (array.Length != 3)
+{
+    Console.WriteLine($"Вы ввели не трёхзначное число. Ваше число содержит {array.Length} знак (-а)!");
+}
+
+if (array.Length == 3)
+{
+Console.WriteLine($"Второй знак введённого числа {array[1]}!");
+}
