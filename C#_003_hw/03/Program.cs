da9e5ded7c80b90f624da294df442fc362447380
Aleksei Grinko <als.grinko@yandex.ru>
2022-12-24 22:42:39 +0400

    make example 3

---
+++ C#_003_hw/03/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 0) 
+{
+    Console.WriteLine($"Число {number} отрицательное!");
+}
+
+else 
+{
+    int[] array = new int[number];
+    for (int i = 1; i <= number; i++)
+    {
+        array[i-1] = Convert.ToInt32(Math.Pow(i , 3));
+    }
+
+    Console.WriteLine($"Кубы чисел от 1 до {number}: ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}");
+        if (i != number - 1)
+        {
+            Console.Write(", ");
+        }
+    }
+    Console.Write(".");
+}
