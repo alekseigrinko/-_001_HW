0ee68aacb39863f0c0ace9cef23be1ce59744299
Aleksei Grinko <als.grinko@yandex.ru>
2022-12-24 22:27:01 +0400

    make example 2

---
+++ C#_003_hw/02/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Введите кооридинаты точки А: ");
+int[] A = new int[3];
+
+A[0] = Convert.ToInt32(Console.ReadLine());
+A[1] = Convert.ToInt32(Console.ReadLine());
+A[2] = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Введены координаты точки А: {A[0]}, {A[1]}, {A[2]}.");
+
+Console.WriteLine("Введите кооридинаты точки B: ");
+int[] B = new int[3];
+
+B[0] = Convert.ToInt32(Console.ReadLine());
+B[1] = Convert.ToInt32(Console.ReadLine());
+B[2] = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Введены координаты точки B: {B[0]}, {B[1]}, {B[2]}.");
+double count = 0;
+for (int i = 0; i < A.Length; i++)
+{
+    count += (Math.Pow(B[i] - A[i], 2));
+}
+double s = Math.Pow(count, (double)1 / 2);
+Console.WriteLine($"Расстояние между введенными координатами равно: {s}.");
