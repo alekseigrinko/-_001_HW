31fc0b0e7118eb54a06a1ad79c8fd4b0148650a0
Aleksei Grinko <als.grinko@yandex.ru>
2023-01-15 10:04:26 +0400

    make example 2

---
+++ C#_004_hw/02/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int sumOfDigits(int numberForSum) {
+    if (numberForSum < 0) {
+        numberForSum *= -1;
+    }
+    return sumArray(numberForSum.ToString().ToCharArray());
+}
+
+int sumArray(char[] array) {
+    int result = 0;
+    for (int i = 0; i < array.Length; i++) {
+        result += (int)Char.GetNumericValue(array[i]);
+    }
+    return result;
+}
+
+Console.WriteLine($"Сумма всех цифр числа {number} равна: {sumOfDigits(number)}. ");
