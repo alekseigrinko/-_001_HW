8907bd0d13a37790f12bccc54fd44dcbf2bec373
Aleksei Grinko <als.grinko@yandex.ru>
2023-01-15 09:28:08 +0400

    make example 1

---
+++ C#_004_hw/01/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите число A: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число B: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+if (numberB < 0) {
+    numberB *= -1;
+    Console.WriteLine($"Число B является отрицательным, будет взято его модульное значение равное {numberB}.");
+}
+
+int exponentiate(int number, int degree) {
+    int result = Convert.ToInt32(Math.Pow(number , degree));
+    return result;
+}
+
+Console.WriteLine($"Число A в степени числа B будет равно: {exponentiate(numberA, numberB)}. ");
