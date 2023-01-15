ccdaeda5b7ca78e2edc982f5a308d7e745e5e43f
Aleksei Grinko <als.grinko@yandex.ru>
2023-01-15 10:26:35 +0400

    make example 3

---
+++ C#_004_hw/03/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите знаечние длины генирируемого массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"[{String.Join(", ", getArray(size))}]");
+
+int[] getArray(int size) {
+    if (size <= 0) {
+        Console.WriteLine("Введено некорректное знаечние размера массива, оно должно быть больше 0!");
+        return new int[0];
+    } else {
+        int[] result = new int[size];
+        for (int i = 0; i < result.Length; i++) {
+            result[i] = new Random().Next(101);
+        }
+        return result;
+    }
+}
+
