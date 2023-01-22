529fa9ceeae658def4bf380e8fc3cec0ed24810a
Aleksei Grinko <als.grinko@yandex.ru>
2023-01-22 16:40:19 +0400

    make example 1

---
+++ C#_007_hw/01/Program.cs
@@ -0,0 +1,15 @@
+﻿getArray(new double[3, 4]);
+
+void getArray(double[,] array)
+{
+    Console.WriteLine($"Сгенерирован двумерный массив случайных вещественных чисел: ");
+    for (int row = 0; row < array.GetLength(0); row++)
+    {
+        for (int column = 0; column < array.GetLength(1); column++)
+        {
+            array[row, column] = new Random().NextDouble() * 40 - 20;
+            Console.Write(string.Format("{0:F2} ", array[row, column]));
+        }
+        Console.WriteLine();
+    }
+}
