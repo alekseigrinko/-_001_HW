0191a625687f85818bd457b2d22655b02c869561
Aleksei Grinko <als.grinko@yandex.ru>
2023-01-22 15:58:43 +0400

    make example 2

---
+++ C#_006_hw/02/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите значение первой прямой: ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine($"Введена первая прямая: b1 {b1}, k1 {k1}.");
+
+Console.WriteLine("Введите значение второй прямой: ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+double k2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine($"Введена вторая прямая: b2 {b2}, k2 {k2}.");
+
+Console.WriteLine($"Точка пересечения двух прямых: [{String.Join(", ", getPoint(b1, k1, b2, k2))}]");
+
+double[] getPoint(double b1, double k1, double b2, double k2) {
+    double x = (b2 - b1) / (k1 - k2);
+    Console.WriteLine(x);
+    double y = k1 * x + b1;
+    return new double[] {y, x};
+}
