3a6ff4ed6ab649b9bb6dc42c93eb3307a5679975
Aleksei Grinko <als.grinko@yandex.ru>
2023-01-29 19:54:13 +0400

    make example 4 with edit

--- C#_008_hw/04/Program.cs
+++ C#_008_hw/04/Program.cs
@@ -3,14 +3,16 @@
 void getArray(int[,,] array)
 {
     Console.WriteLine($"Сгенерирован трёхмерный массив случайных чисел: ");
+    int number = 9;
     for (int x = 0; x < array.GetLength(2); x++)
     {
         for (int i = 0; i < array.GetLength(0); i++)
         {
             for (int j = 0; j < array.GetLength(1); j++)
             {
-                array[i, j, x] = new Random().Next(10, 100);
+                array[i, j, x] = new Random().Next(number + 1, number + 6);
                 Console.Write($"{array[i, j, x]} ({i}, {j}, {x}) ");
+                number += 6;
             }
             Console.WriteLine();
         }
