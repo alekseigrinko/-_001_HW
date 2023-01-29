f2e4456b56148911ff680c40899111067a73be9c
Aleksei Grinko <als.grinko@yandex.ru>
2023-01-29 20:06:01 +0400

    make example 1

---
+++ C#_009_hw/01/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число N: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Все натуральные числа в промежутке от N до 1: ");
+getNumber(number);
+void getNumber(int number) {
+
+    if (number == 1) {
+        Console.Write("1");
+    } else {
+        Console.Write($"{number}, ");
+        getNumber(number - 1);
+    }
+}
