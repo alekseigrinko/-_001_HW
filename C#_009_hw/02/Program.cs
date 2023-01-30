e48c1f4ed9f768bf2f8fe3100468eb96ea51262e
Aleksei Grinko <als.grinko@yandex.ru>
2023-01-30 19:43:18 +0400

    make example 2

---
+++ C#_009_hw/02/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число M: ");
+int numberM = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число N: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Сумма натуральных элементов в промежутке от M до N: {getSumNumber(numberM, numberN)}.");
+int getSumNumber(int M, int N)
+{
+    if (M == N) {
+        return M;
+    } 
+    return M + getSumNumber(M+1, N);
+}
