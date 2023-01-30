5f951d3caf4f5e694aed043fb80605f6faf8d564
Aleksei Grinko <als.grinko@yandex.ru>
2023-01-30 20:00:54 +0400

    make example 3

---
+++ C#_009_hw/03/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите неотрицательное число m: ");
+int numberM = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите неотрицательное n: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Функция Аккермана двух неотрицательных чисел m и n: {function(numberM, numberN)}.");
+int function(int m, int n)
+{
+    if (m == 0) {
+        return n + 1;
+    } else if ((m != 0) && (n == 0)) {
+        return function(m -1, 1);
+    } else {
+        return function(m - 1, function(m, n - 1));
+    }
+}
